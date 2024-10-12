895de7a2e3fe39139c5b319861f9ab0093074a31
KarenEhab53 <karenehab53@gmail.com>
2024-10-12 18:13:57 +0300

    last edition

--- src/Components/Home/Section Three/SectionThree.js
+++ src/Components/Home/Section Three/SectionThree.js
@@ -1,6 +1,7 @@
 import img1 from '../../../images/unsplash_v8XaVfyo41Q.f3e57dd7d7497b936f0c.png';
 import img2 from '../../../images/image 1.86da83f890d792e2a4ae.png';
 import './sectionthree.css';
+import { Link } from 'react-router-dom';
 function SectionThree() {
     return ( <>
     <div className="sectionthree">
@@ -8,7 +9,7 @@ function SectionThree() {
             <div className="data">
                 <h1>Enhanse Your Game Experience</h1>
                 <p>Best Experience With Most <br/> famous games you can find now !</p>
-                <button>Explore Now</button>
+                <Link to={'/filteration'}><button>Explore Now</button></Link>
             </div>
             <div className="images">
                 <div className="img1"><img src={img1} alt=''/></div>
