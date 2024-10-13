85ab73f3df199e708abd9d6bb5fbae60d47ffe7e
johnsame1 <129014486+johnsame1@users.noreply.github.com>
2024-10-13 14:03:43 +0300

    handleFile

--- src/Pages/Filteration.js
+++ src/Pages/Filteration.js
@@ -1,7 +1,7 @@
-import SectionOne from '../Components/SectionOne/file/SectionOne';
-import ProductList from '../Components/categories/Categories';
-import NewCategory from '../Components/NewCategory/NewCategory';
-import FreshSale from '../Components//FreshSale/FreshSale';
+import SectionOne from '../Components/Filteration/SectionOne/file/SectionOne';
+import ProductList from '../Components/Filteration/categories/Categories';
+import NewCategory from '../Components/Filteration/NewCategory/NewCategory';
+import FreshSale from '../Components/Filteration/FreshSale/FreshSale';
 
 
 function Home(){
