312644d15adcc6388812dc75a2529352aeebd997
Marccc <129010043+Marcc88@users.noreply.github.com>
2024-11-02 14:39:59 +0200

    mm

--- src/Components/axios/Axios.js
+++ src/Components/axios/Axios.js
@@ -2,7 +2,6 @@
 
 import axios from 'axios';
 
-// Create an instance of axios with the default baseURL
 const axiosInstance = axios.create({
   baseURL: 'http://194.164.77.238:8001/api', // Set the default base URL
 
