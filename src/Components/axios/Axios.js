4f8878fcb48efdf56c36cf8f37e31b4a8f95eea9
KarenEhab53 <karenehab53@gmail.com>
2024-11-06 23:26:23 +0200

    fake api

--- src/Components/axios/Axios.js
+++ src/Components/axios/Axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the default baseURL
 const axiosInstance = axios.create({
-  baseURL: 'http://194.164.77.238:8001/api', // Set the default base URL
+  baseURL: 'https://fakestoreapi.com/products', // Set the default base URL
  
 });
 export default axiosInstance;
